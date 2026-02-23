8736f9382cc6aab960659b875764861d5bb5a51b
JavaAshwini <60837528+JavaAshwini@users.noreply.github.com>
2026-02-23 01:42:53 +0000

    Update Hooks.java

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,5 +1,7 @@
-package hooks;
+package Hooks;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,5 +9,17 @@ public class Hooks {
 
     private static final Logger log =
             LoggerFactory.getLogger(Hooks.class);
+
+    @Before("@Regression")
+    public void beforeScenario() {
+        System.out.println("---------------Running the Before scenario---------------");
+    }
+
+    @After(order = 1)
+    public void afterScenario() {
+        System.out.println("---------------Scenario completed----------------");
+    }
+
+
 }
 
