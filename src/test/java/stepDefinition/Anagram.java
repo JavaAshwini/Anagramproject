8895f3ca6b9f6e113407aa1c6321d8d4bf7df0aa
JavaAshwini <60837528+JavaAshwini@users.noreply.github.com>
2026-02-23 01:43:58 +0000

    Update Anagram.java

--- src/test/java/stepDefinition/Anagram.java
+++ src/test/java/stepDefinition/Anagram.java
@@ -1,35 +1,45 @@
-package stepDefinition;
+package StepDefinition;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import utils.AnagramUtils;
-
+import steps.AnagramSteps;
 
 public class Anagram {
-    boolean isAnagram = false;
-    private String str1, str2;
+    /// Instance of AnagramSteps used to perform anagram logic
+    private final AnagramSteps anagramSteps = new AnagramSteps();
 
+    /**
+     * Captures the input strings provided in the Given step.
+     *
+     * @param input1 first input string
+     * @param input2 second input string
+     */
     @Given("^the input strings \"([^\"]*)\" and \"([^\"]*)\"$")
     public void the_input_strings_and(String input1, String input2) {
         System.out.println("[STEP] Given input strings: " + input1 + " , " + input2);
-        this.str1 = input1;
-        this.str2 = input2;
+        anagramSteps.setFirstInput(input1);
+        anagramSteps.setSecondInput(input2);
     }
 
+    /**
+     * Executes the anagram check using the provided inputs.
+     */
     @When("I check if there are anagrams")
     public void i_check_if_there_are_anagrams() {
         System.out.println("[STEP] When checking if strings are anagrams");
-        isAnagram = AnagramUtils.checkAnagram(str1, str2);
+        anagramSteps.checkAnagram();
     }
 
-
-    @Then("the result should be {string}")
-    public void theResultShouldBe(String arg0) {
-        System.out.println("[STEP] Then expected result: " + arg0 + ", actual: " + isAnagram);
-        Boolean result = Boolean.parseBoolean(arg0);
-        Assert.assertEquals(isAnagram, result);
+    /**
+     * Verifies whether the actual result matches the expected result.
+     */
+    @Then("^the result should be \"([^\"]*)\"$")
+    public void theResultShouldBe(boolean expected) {
+        boolean result = anagramSteps.getResult();
+        System.out.println("[STEP] Then expected result: " + expected + ", actual: " + result);
+        Assert.assertEquals(result, expected);
     }
 
 
