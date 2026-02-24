bbc9654313d742bed1bf7445ac2ccef3e861ac2c
Ash Java <ashwinispawar2005@gmail.com>
2026-02-24 22:45:15 +0000

    Code for anagram checks

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -2,9 +2,13 @@ package Hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Hooks {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(Hooks.class);
 
     @Before("@Regression")
     public void beforeScenario() {
